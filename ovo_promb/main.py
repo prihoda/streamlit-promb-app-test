7d07f0d383bdafb8308be27c1161073159ee6768
David Prihoda <david.prihoda@msd.com>
2026-05-08 14:50:14 +0200

    Download java in demo

--- ovo_promb/main.py
+++ ovo_promb/main.py
@@ -5,6 +5,8 @@ import subprocess
 
 st.set_page_config(layout="wide", page_title="promb", page_icon="🔥")
 
+is_streamlit_cloud = os.environ.get("HOSTNAME") == "streamlit"
+
 TEST_HOME_DIR = "/tmp/ovo"
 if not os.path.exists(TEST_HOME_DIR):
     with st.spinner("Initializing OVO..."):
@@ -12,22 +14,30 @@ if not os.path.exists(TEST_HOME_DIR):
         subprocess.run(["ovo", "init", "home", TEST_HOME_DIR, "-y", "--no-env"])
         assert os.path.exists(os.path.join(TEST_HOME_DIR, "config.yml")), "OVO init home failed"
 
+        if is_streamlit_cloud:
+            subprocess.run(["curl", "-L", "-o", "/tmp/openjdk.tar.gz",
+                            "https://download.java.net/java/GA/jdk21/fd2272bbf8e04c3dbaee13770090416c/35/GPL/openjdk-21_linux-x64_bin.tar.gz"])
+            subprocess.run(["tar", "-xzf", "-C", "/tmp", "/tmp/openjdk.tar.gz"])
+
+if is_streamlit_cloud:
+    os.environ["PATH"] = "/tmp/jdk-21:" + os.environ["PATH"]
+
 os.environ["OVO_HOME"] = TEST_HOME_DIR
 
-st.subheader("Run shell command")
-with st.form(key="run_command", border=False):
-    shell_command = st.text_area("Enter command:", placeholder="ls -la", key="shell_command", height=80)
-    if st.form_submit_button("Run"):
-        with st.spinner("Running command..."):
-            result = subprocess.run(
-                shell_command,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.STDOUT,
-                text=True,
-                shell=True,
-            )
-        st.write("Finished with output:" if result.returncode == 0 else "Finished with error:")
-        st.code(result.stdout)
+# st.subheader("Run shell command")
+# with st.form(key="run_command", border=False):
+#     shell_command = st.text_area("Enter command:", placeholder="ls -la", key="shell_command", height=80)
+#     if st.form_submit_button("Run"):
+#         with st.spinner("Running command..."):
+#             result = subprocess.run(
+#                 shell_command,
+#                 stdout=subprocess.PIPE,
+#                 stderr=subprocess.STDOUT,
+#                 text=True,
+#                 shell=True,
+#             )
+#         st.write("Finished with output:" if result.returncode == 0 else "Finished with error:")
+#         st.code(result.stdout)
 
 from ovo import db
 from ovo.core.utils.tests import create_test_project_data
